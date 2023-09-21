6f23b340f7ebed2826f575e13ac12a643c17e24f
Fluubah <youngsonaaron@gmail.com>
2023-09-21 10:29:45 -0500

    New Stack Layout, and solution for Dead space

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.stacklayout import StackLayout
 
 
 class BoxLayoutExample(BoxLayout):
@@ -11,9 +12,14 @@ class MainWidget(Widget):
     pass
 
 
+class StackLayoutExample(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "lr_tb"
+
+
 class WidgetLayoutApp(App):
     pass
-
 
 
 WidgetLayoutApp().run()
